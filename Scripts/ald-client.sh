16ab8f3aa631ac01856a2529c1a8c74ce62d6a37
IB-skills <97691735+IB-skills@users.noreply.github.com>
2022-02-14 21:29:20 +0500

    Create ald-client.sh

---
+++ Scripts/ald-client.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+#Задаем переменные
+con="Проводное соединение 1"
+ip="192.168.10.10"
+mask="/24"
+gw="192.168.10.1"
+dns="192.168.10.100"
+
+# Задаем адрес шлюза
+nmcli con mod "$con" ip4 $ip$mask gw4 $gw
+
+# Задаем адреса DNS
+nmcli con mod "$con" ipv4.dns "$dns"
+
+# Отключаем DHCP, переводим в "ручной" режим настройки
+nmcli con mod "$con" ipv4.method manual
+nmcli con mod "$con" ipv6.method ignore
+nmcli -p con show "$con" | grep ipv4
+
+# Перезапускаем соединение для применения новых настроек
+nmcli con down "$con" ; nmcli con up "$con"
+
+#Пользователь
+
+#Устанавливаем пакеты
+apt install ald-client-common ald-admin fly-admin-ald-client ald-client -y
+
+#Обновляем пакеты
+apt -f install -y
+
+#Вводи полное доменное имя
+hostnamectl set-hostname iwdm
+
+#НАстройки сети
+echo -n > /etc/hosts
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "192.168.10.100  ad.demo.lab     ad" >> /etc/hosts
+echo "$ip   iwdm.demo.lab   iwdm" >> /etc/hosts
+
+#Для тестов
+#ald-client status
+
+#Входим в домен
+ald-client join
+
+#Перезагрузка
+echo "Please reboot"
