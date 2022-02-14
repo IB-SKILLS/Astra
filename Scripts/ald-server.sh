a5b8778ef6d4aca071a56df34fdb0457e7924231
IB-skills <97691735+IB-skills@users.noreply.github.com>
2022-02-14 21:32:23 +0500

    Create ald-server.sh

---
+++ Scripts/ald-server.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+#Settable initial parameters
+con="Проводное соединение 1"
+ip="192.168.10.10"
+mask="/24"
+gw="192.168.10.1"
+dns="192.168.10.100"
+
+# Задаем адрес шлюза
+nmcli con mod "$con" ip4 $ip$mask gw4 $gw
+
+# Задаем адреса DNS
+nmcli con mod "$con" ipv4.dns "$dns"
+
+# Отключаем DHCP, переводим в "ручной" режим настройки
+nmcli con mod "$con" ipv4.method manual
+nmcli con mod "$con" ipv6.method ignore
+nmcli -p con show "$con" | grep ipv4
+
+# Перезапускаем соединение для применения новых настроек
+nmcli con down "$con" ; nmcli con up "$con"
+
+#Сервер
+
+#Установка пакетов
+apt install fly-admin-ald-server ald-server-common smolensk-security-ald -y
+
+#Обновление пакетов
+apt -f install -y
+
+#Вводи полное доменное имя
+hostnamectl set-hostname ad.demo.lab
+
+#НАстройка сети
+echo -n > /etc/hosts
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "192.168.10.100  ad.demo.lab     ad" >> /etc/hosts
+echo "192.168.10.20   iwdm.demo.lab   iwdm" >> /etc/hosts
+
+#Тестировка
+#ald-admin test-integrity
+#ald-client status
+
+#Перезагрузка
+echo "Please reboot and set up the domain"
