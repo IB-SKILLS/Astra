821b956214b33f5787c6956d13dff4ac6a5f148a
fruworg <96399261+fruworg@users.noreply.github.com>
2022-02-21 11:10:15 +0500

    Update client-ad.sh

--- Scripts/client-ad.sh
+++ Scripts/client-ad.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 if [[ "$1" != "" ]]; then
 if [[ $(whoami) == "root" ]]; then
+
+#Имя соединения
+# $2 - IP, $3 - Маска, $4 - Gateway, $5 - DNS
+con="Проводное соединение 1"
+
 #Установка пакетов
 apt install astra-ad-sssd-client -y
 
 #Вводим краткое доменное имя
 hostnamectl set-hostname "$1"
+
+# Задаем адрес шлюза
+nmcli con mod "$con" ip4 $2$3 gw4 $4
+
+# Задаем адреса DNS
+nmcli con mod "$con" ipv4.dns "$5"
+
+# Отключаем DHCP, переводим в "ручной" режим настройки
+nmcli con mod "$con" ipv4.method manual
+nmcli con mod "$con" ipv6.method ignore
+nmcli -p con show "$con" | grep ipv4
+
+# Перезапускаем соединение для применения новых настроек
+nmcli con down "$con" ; nmcli con up "$con"
 
 #Вход в домен Active Directory
 astra-ad-sssd-client -d demo.lab -u Administrator -p xxXX1234 -y
