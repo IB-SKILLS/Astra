57b7892c3c8d56e527b62e25ec4fd0dcddcf7647
fruworg <96399261+fruworg@users.noreply.github.com>
2022-02-21 10:56:49 +0500

    Update client-ad.sh

--- Scripts/client-ad.sh
+++ Scripts/client-ad.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
+if [[ "$1" != "" ]]; then
 
 #Установка пакетов
 apt install astra-ad-sssd-client -y
 
 #Вводим краткое доменное имя
-hostnamectl set-hostname iwdm
+hostnamectl set-hostname "$1"
 
 #Вход в домен Active Directory
 astra-ad-sssd-client -d demo.lab -u Administrator -p xxXX1234 -y
 
 #Перезагрузка
-echo "Please reboot"
+read -p 'Перезагрузить ПК? ' in
+if [[ "$in" == "y" ]]; then
+sudo reboot
+fi
+
+#Забыл имя ПК
+else
+echo "Ты забыл про имя ПК!"
+fi
