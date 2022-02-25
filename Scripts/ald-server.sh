90b0c9ea8239224c9b828511ee93cf6986b3cf70
IB-SKILLS <97691735+IB-SKILLS@users.noreply.github.com>
2022-02-25 09:39:34 +0500

    Update ald-server.sh

--- Scripts/ald-server.sh
+++ Scripts/ald-server.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-#Задаем переменные для настройки сети
+#Имя соединения
+# $2 - IP, $3 - Mask, $4 - Gateway, $5 - DNS
 con="Проводное соединение 1"
-ip="192.168.10.200"
-mask="/24"
-gw="192.168.10.1"
-dns="192.168.10.1"
+
+#Установка пакетов
+apt install astra-ad-sssd-client -y
+
+#Вводим краткое доменное имя
+hostnamectl set-hostname "$1"
 
 # Задаем адрес шлюза
-nmcli con mod "$con" ip4 $ip$mask gw4 $gw
+nmcli con mod "$con" ip4 $2/$3 gw4 $4
 
 # Задаем адреса DNS
-nmcli con mod "$con" ipv4.dns "$dns"
+nmcli con mod "$con" ipv4.dns "$5"
 
 # Отключаем DHCP, переводим в "ручной" режим настройки
 nmcli con mod "$con" ipv4.method manual
